c538f347bdc95ea4bcc1f5a17f72af0a3d64a27d
Bernard Kwok <kwokcb@gmail.com>
2025-01-16 11:58:35 -0500

    Add build for Substance and QuiltiX (GPUOpen) examples.

--- utilities/build_examples.sh
+++ utilities/build_examples.sh
@@ -6,3 +6,15 @@ python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geomet
 python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf ./Tire.mtlx --translateShaders True --bakeTextures True
 python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf ./Whiteboard.mtlx --translateShaders True --bakeTextures True
 popd
+pushd .
+cd docs/data/Substance3D
+python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_cube.gltf parquet_clothes.mtlx
+python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf parquet_clothes.mtlx --gltfFileName parquet_clothes.mtlx_sphere.gltf
+rm parquet_clothes.mtlx_sphere.gltf
+popd
+pushd .
+cd docs/data/QuiltiX
+python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf Black_Upholstery.mtlx_baked.mtlx
+python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf .//Copper_Old.gltf_baked.mtlx
+python -m materialxgltf mtlx2gltf --packageBinary 1 --gltfGeomFileName ../Geometry/test_sphere.gltf .//Motley_Patchwork_Rug.mtlx
+popd
